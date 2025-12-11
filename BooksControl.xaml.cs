2076b7babd3e5ee8fb69e9c6547841532b78312e
Nikola Kihas <104202675+KikicaBacaTrice@users.noreply.github.com>
2025-12-11 16:23:47 +0100

    Add files via upload

---
+++ BooksControl.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dns
+{
+    /// <summary>
+    /// Interaction logic for BooksControl.xaml
+    /// </summary>
+    public partial class BooksControl : UserControl
+    {
+        public event Action<int> BookSelected;
+        public BooksControl()
+        {
+            InitializeComponent();
+            dgBooks.SelectionChanged += dgBooks_SelectionChanged;
+
+        }
+
+        private void dgBooks_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (dgBooks.SelectedItem is Book selectedBook)
+            {
+               int id = selectedBook.Id;
+                BookSelected?.Invoke(id);
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dns
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly BooksControl booksControl;
+        private readonly AuthorControl authorControl;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            booksControl = new BooksControl();
+            booksControl.BookSelected += OnBookSelected;
+            booksContent.Content = booksControl;
+
+            authorControl = new AuthorControl();
+            authorContent.Content = authorControl;
+        }
+
+        private void OnBookSelected(int bookId)
+        {
+            using(var db = new Model1())
+            {
+                var author= db.Authors.FirstOrDefault((a) => a.Books.Any(b=> b.Id == bookId));
+                if(author != null)
+                {
+                    authorControl.txtSelectedBook.Text = db.Books.Find(bookId).Title;
+                    authorControl.txtAuthor.Text = author.Name;
+                    authorControl.txtBio.Text = author.Bio;
+                }
+            }
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            using(var db = new Model1())
+            {
+                booksControl.dgBooks.ItemsSource = db.Books.ToList();
+            }
+        }
+    }
+}
